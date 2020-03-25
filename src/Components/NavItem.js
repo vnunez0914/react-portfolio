e04372c71f271cd258e61148fa48b3c7dc463c78
Victor Nunez <vnunez0914@gmail.com>
2020-03-24 19:42:35 -0500

    removed Route which was causing nav links to show double

--- src/Components/NavItem.js
+++ src/Components/NavItem.js
@@ -1,21 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // Link is imported from react-router-dom npm package and acts as an <a>
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class NavItem extends Component {
-    render() {
-
-        //  setting "item" and "tolink" props to the Link component.
-        return (
-            <li id={this.props.item}>
-                <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)} >{this.props.item}
-                    {this.props.item}
-                </Link>
-            </li>
-           
-        )
-    }
+class Navitem extends Component {
+  render() {
+    //  setting "item" and "tolink" props to the Link component.
+    return (
+      <li id={this.props.item}>
+        <Link to={this.props.tolink} onClick={this.props.activec.bind(this, this.props.item)} >
+             {this.props.item}
+        </Link>
+      </li>
+    );
+  }
 }
-
-
-export default NavItem
+export default Navitem;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,41 +1,39 @@
-import React, {Component} from 'react';
-import NavItem from './NavItem';
+import React, { Component } from "react";
+import Navitem from "./Navitem";
 
-class NavBar extends Component {
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            'NavItemActive': ''
-        }
-    }
-        // activeitem() will be sent to NavItem component and wiil be called whenever NavItem "activec" is clicked
-    activeitem=(x)=>{
-        if(this.state.NavItemActive.length > 0){
 
-            document.getElementById(this.state.NavItemActive).classList.remove('active');
 
-        }
-
-        this.setState({'NavItemId':x},()=>{
-            document.getElementById(this.state.NavItemActive).classList.add('active');
-        });
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      NavItemActive: ""
     };
-    render(){
-
-        return(
-            <nav>
-                {/* NavItem holds two props (item and tolink). "item" holds the nav name and "tolink" prop holds the nav route */}
-                <ul>
-                    <NavItem item="Home" tolink="/" activec={this.activeitem} ></NavItem>
-                    <NavItem item="About" tolink="/about" activec={this.activeitem} ></NavItem>
-                    <NavItem item="Education" tolink="/education" activec={this.activeitem} ></NavItem>
-                    <NavItem item="Skills" tolink="/skills" activec={this.activeitem} ></NavItem>
-                    <NavItem item="Contact" tolink="/contact" activec={this.activeitem}></NavItem>
-                </ul>
-            </nav>
-        )
+  }
+  // activeitem() will be sent to NavItem component and wiil be called whenever NavItem "activec" is clicked
+  activeitem = x => {
+    if (this.state.NavItemActive.length > 0) {
+      document
+        .getElementById(this.state.NavItemActive)
+        .classList.remove("active");
     }
+    this.setState({ NavItemId: x }, () => {
+      document.getElementById(this.state.NavItemActive).classList.add("active");
+    });
+  };
+  render() {
+    return (
+      <nav>
+        {/* NavItem holds two props (item and tolink). "item" holds the nav name and "tolink" prop holds the nav route */}
+        <ul>
+          <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
+          <Navitem item="About" tolink="/about" activec={this.activeitem} ></Navitem>
+          <Navitem item="Education" tolink="/education" activec={this.activeitem} ></Navitem>
+          <Navitem item="Skills" tolink="/skills" activec={this.activeitem} ></Navitem>
+          <Navitem item="Contact" tolink="/contact" activec={this.activeitem} ></Navitem>
+        </ul>
+      </nav>
+    );
+  }
 }
-
-export default NavBar
+export default Navbar;

1f28d872e504818a99cfbd4ecfc9fbcd1c554d61
Victor Nunez <vnunez0914@gmail.com>
2020-03-25 17:28:01 -0500

    fixed Home.js className was missing for css

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -6,7 +6,7 @@ import Social from '../Components/Social'
 class Home extends Component {
     render() {
         return(
-            <div className="condivhome">
+            <div className="condiv home">
                 <img src={profilepic} className="profilepic"></img>
 
                 <ReactTypingEffect className="typingeffect" text={['I am Victor Nunez', 'I am a Fullstack Web Developer.']} speed={100} eraseDelay={700}/>

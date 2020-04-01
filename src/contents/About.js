3269ecd016de87e8ac490527bb26aa40efd0938e
Victor Nunez <vnunez0914@gmail.com>
2020-04-01 13:38:29 -0500

    about me edit

--- src/contents/About.js
+++ src/contents/About.js
@@ -1,30 +1,35 @@
-import React, {Component} from  'react';
+import React, { Component } from "react";
 
 class About extends Component {
-    render() {
-        return (
-            <div className="condiv">
-                <h1 className="subtopic">About Me</h1>
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">About Me</h1>
 
-                <h4>Hello!</h4>
+        <h4>Hello!</h4>
 
-                <h1>I am Victor Nunez</h1>
+        <h1>I am Victor Nunez</h1>
 
-                <h3>FullStack Web Developer</h3>
+        <h3>FullStack Web Developer</h3>
 
-                <br></br>
+        <br></br>
 
-                <p>Full Stack Developer specializing in the MERN Stack: Mongo, Express, React, Node. 
-                 I enjoy improving my code and learning new technology with each project I create. My journey started at the Northwestern University Fullstack Web Development Bootcamp. 
-                 After finishing the program I found a new interest and enjoy the task that come with development. Aside from having coding experience, I have a passion for learning, helping others and personal growth. 
-                 I am very self-motivated and I am looking forward to pursuing a career in Development.
-
-                </p>
-
-
-            </div>
-        )
-    }
+        <p>
+          Full Stack Developer specializing in the MERN Stack: Mongo, Express,
+          React, Node. I enjoy improving my code and learning new technology
+          with each project I create. My journey started at the Northwestern
+          University Fullstack Web Development Bootcamp. After concluding the
+          program, I came to better appreciate and enjoy the challenges and
+          satisfaction that come with development. In addition to gaining
+          valuable coding experience, I also discovered a passion for learning,
+          helping others navigate the challenges of coding and thereby achieving
+          personal growth. I am self-motivated, I believe in commitment to my
+          goals and perseverance and I look forward to pursuing a career in
+          Development.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;

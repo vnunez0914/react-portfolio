3a69ccdc5a255bfc668f66f7ae18ee29ed64b008
Victor Nunez <vnunez0914@gmail.com>
2020-05-05 13:46:02 -0500

    Updates

---
+++ precache-manifest.35cc0e9dc2f5c688cc4e2d50516d10bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6853450d4dde461810c24f3b411fdb5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ad311999be35f16c61a8",
+    "url": "/react-portfolio/static/css/main.ed80c30c.chunk.css"
+  },
+  {
+    "revision": "923bf85e98b3339fdbbb",
+    "url": "/react-portfolio/static/js/2.841c2529.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/react-portfolio/static/js/2.841c2529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad311999be35f16c61a8",
+    "url": "/react-portfolio/static/js/main.7464b25f.chunk.js"
+  },
+  {
+    "revision": "7d28034c6e7746429169",
+    "url": "/react-portfolio/static/js/runtime-main.16748238.js"
+  },
+  {
+    "revision": "edb8f1def8e4af171ffd5fa871cda56d",
+    "url": "/react-portfolio/static/media/portfolioPhoto.edb8f1de.JPEG"
+  }
+]);
